74d8f4f20202fe29d0518fbf0b1ffceda9d637dc
Harry C <harrychenhc@icloud.com>
2019-05-05 16:09:33 -0400

    was able to implement game of life WITH A LOT OF HELP

--- game-of-life.js
+++ game-of-life.js
@@ -1,8 +1,3 @@
-// Any live cell with two or three live neighbors lives on to the next generation.
-// Any live cell with fewer than two live neighbors dies, as if caused by under-population.
-// Any live cell with more than three live neighbors dies, as if by overcrowding.
-// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-
 /* 
   typical case to worry about (cell is at a location that has 8 cells around it)
   [0, 1, 0]
@@ -35,24 +30,69 @@ class GameOfLife {
   }
   tick(){
     const newBoard = this.makeBoard()
-    
-
-
-
+    this.board.forEach((row, rowIdx) => {
+      const newRow = row.reduce((newRow, cell, colIdx) => {
+        newRow.push(this.cellCheck(cell, rowIdx, colIdx))
+        return newRow
+      }, [])
+      newBoard[rowIdx] = newRow
+    })
+    this.board = newBoard
   }
   makeBoard(){
     const board = []
-    for(let i = 1; i <= this.height; i++){
+    for(let i = 0; i < this.height; i++){
       const row = []
-      for(let j = 1; j <= this.width; j++){
+      for(let j = 0; j < this.width; j++){
        row.push(0)
       }
       board.push(row)
     }
     return board
   }
+  existOrNot(row, col){
+    if(!this.board[row]){ //if you get an element out of a row that's non existent then an error will be thrown
+      return;             //so return nothing so our checking condition will work
+    } else {
+      return this.board[row][col] //if the column doesn't exist no error would be thrown only a returned value of undefined which we could *1 to check if it's a number or NaN
+    }
+  }
   livingNeighbors(row, col){ //checks the cells coordinates for all living neighbors near it to determine if current cell lives or dies
-
+    const neighbors = [
+      [row + 1, col - 1],
+      [row + 1, col],
+      [row + 1, col + 1],
+      [row, col - 1],
+      [row, col + 1],
+      [row - 1, col - 1],
+      [row - 1, col],
+      [row - 1, col + 1]
+    ]
+    return neighbors.reduce((neighbors, position) => {
+      if(this.existOrNot(position[0], position[1])*1 === 1){
+        neighbors++
+      }
+      return neighbors
+    }, 0)
+  }
+  cellCheck(alive, row, col){
+    let neighbors = 0
+    if(alive){
+      neighbors = this.livingNeighbors(row, col)
+      return neighbors > 1 && neighbors <= 3? 1 : 0
+    } else {
+      neighbors = this.livingNeighbors(row, col)
+      return neighbors === 3? 1 : 0
+    }
+  }
+  randomize(){
+    this.board = this.board.reduce((board, row) => {
+      for(let i = 0; i < row.length; i++){
+        row[i] = Math.round(Math.random())
+      }
+      board.push(row)
+      return board
+    }, [])
   }
   clearBoard(){
     this.board = this.makeBoard()
--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const gol = new GameOfLife(height, width)
 const tds = []
 
 const table = document.createElement('tbody')
-for(let i = 1; i <= height; i++){
+for(let i = 0; i < height; i++){
   const row = document.createElement('tr')
-  for(let j = 1; j <= width; j++){
+  for(let j = 0; j < width; j++){
     const cell = document.createElement('td')
     cell.dataset.row = i  //dataset property of an element allows up to put in data we need which we'll use when a user choose the starting nodes to start the game of life
     cell.dataset.col = j  //when they click a node, thier row and col positions will be set back up so we can change those to "alive"
@@ -18,35 +18,60 @@ for(let i = 1; i <= height; i++){
 }
 
 const toggleAlive = element => {
-  // console.log(element.dataset.row)
-  // console.log(element.dataset.col) 
-  element.classList.contains('alive')? gol.board[element.dataset.row - 1][element.dataset.col - 1] = 0 : gol.board[element.dataset.row - 1][element.dataset.col - 1] = 1
-  element.classList.contains('alive')? element.classList.remove('alive') : element.classList.add('alive') 
+  element.classList.contains('alive')? gol.board[element.dataset.row ][element.dataset.col] = 0 : gol.board[element.dataset.row][element.dataset.col] = 1
 }
+
+const paint = () => {
+  tds.forEach((td) => {
+    const { row, col } = td.dataset
+    if(gol.board[row][col]){
+      td.classList.add('alive')
+    } else {
+      td.classList.remove('alive')
+    }
+  })
+}
+
 document.getElementById('board').append(table)
 
 document.getElementById('board').addEventListener('click', event => {
   if(event.srcElement.tagName === 'TD'){
-    return toggleAlive(event.srcElement)
+    toggleAlive(event.srcElement)
+    paint()
   }
 })
 
-document.getElementById('pause').addEventListener('click', event => {
-
+let interval;
+document.getElementById('start').addEventListener('click', event => {
+  if(!interval){
+    interval = setInterval(() => {
+      gol.tick()
+      paint()
+      let aliveCells = tds.some(cell => {
+        return cell.classList.contains('alive')
+      })
+      if(!aliveCells){
+        clearInterval(interval)
+        interval = 0 
+      }
+    }, 100)
+  } else {
+    clearInterval(interval)
+    interval = 0
+  }
 })
 
 document.getElementById('one-stop').addEventListener('click', event => {
-
+  gol.tick()
+  paint()
 })
 
 document.getElementById('random').addEventListener('click', event => {
-
+  gol.randomize()
+  paint()
 })
 
 document.getElementById('clear').addEventListener('click', event => {
-  for(let i = 0; i < tds.length; i++){
-    const cell = tds[i]
-    cell.classList.remove('alive')
-  }
   gol.clearBoard()
+  paint()
 })
