f7527a202631a77d96788907d12b09979cfa3e3f
Harry C <harrychenhc@icloud.com>
2019-05-04 23:09:59 -0400

    users can set cells alive or kill them and have the game state be updated as well

---
+++ game-of-life.js
@@ -0,0 +1,58 @@
+// Any live cell with two or three live neighbors lives on to the next generation.
+// Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+// Any live cell with more than three live neighbors dies, as if by overcrowding.
+// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+/* 
+  typical case to worry about (cell is at a location that has 8 cells around it)
+  [0, 1, 0]
+  [1, cell, 1]
+  [0, 0, 1]
+*/
+/* Edge cases (cells in awkward sitations with less that 8 cells around it) 
+
+  1) Cell is at the outer corner of the board
+  [cell, 1] 
+  [0, 1]
+
+  2) Cell is at the top or bottom row of the table but not the edges
+  [1, cell, 0]
+  [0, 1, 1]
+
+  3) Cell is at the beginning of a column or end but not near edge
+  [0, 1]
+  [cell, 1]
+  [1, 0]
+
+*/
+
+class GameOfLife {
+  constructor(height, width){
+    this.height = height
+    this.width = width
+    this.board = this.makeBoard()
+
+  }
+  tick(){
+
+  }
+  makeBoard(){
+    const board = []
+    for(let i = 1; i <= this.height; i++){
+      const row = []
+      for(let j = 1; j <= this.width; j++){
+        const neighbors = this.livingNeighbors(i, j)
+        if(neighbors > 1 && neighbors <= 3){
+          row.push(1)
+        } else {
+          row.push(0)
+        }
+      }
+      board.push(row)
+    }
+    return board
+  }
+  livingNeighbors(row, col){ //checks the cells coordinates for all living neighbors near it to determine if current cell lives or dies
+
+  }
+}
---
+++ main.js
@@ -0,0 +1,48 @@
+const height = 20
+const width = 20
+const gol = new GameOfLife(height, width)
+
+const tds = []
+
+const table = document.createElement('tbody')
+for(let i = 1; i <= height; i++){
+  const row = document.createElement('tr')
+  for(let j = 1; j <= width; j++){
+    const cell = document.createElement('td')
+    cell.dataset.row = i  //dataset property of an element allows up to put in data we need which we'll use when a user choose the starting nodes to start the game of life
+    cell.dataset.col = j  //when they click a node, thier row and col positions will be set back up so we can change those to "alive"
+    row.append(cell)
+    tds.push(cell)
+  }
+  table.append(row)
+}
+
+const toggleAlive = element => {
+  // console.log(element.dataset.row)
+  // console.log(element.dataset.col) 
+  element.classList.contains('alive')? gol.board[element.dataset.row - 1][element.dataset.col - 1] = 0 : gol.board[element.dataset.row - 1][element.dataset.col - 1] = 1
+  element.classList.contains('alive')? element.classList.remove('alive') : element.classList.add('alive') 
+}
+document.getElementById('board').append(table)
+
+document.getElementById('board').addEventListener('click', event => {
+  if(event.srcElement.tagName === 'TD'){
+    return toggleAlive(event.srcElement)
+  }
+})
+
+document.getElementById('pause').addEventListener('click', event => {
+
+})
+
+document.getElementById('one-stop').addEventListener('click', event => {
+
+})
+
+document.getElementById('random').addEventListener('click', event => {
+
+})
+
+document.getElementById('clear').addEventListener('click', event => {
+
+})
